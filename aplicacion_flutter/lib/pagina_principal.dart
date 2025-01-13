6aa74a9e0563d2b062053a1201fe4091dcd65ea2
ireneferrandez <133006045+ireneferrandez@users.noreply.github.com>
2025-01-13 20:12:15 +0100

    actualizacion

--- aplicacion_flutter/lib/pagina_principal.dart
+++ aplicacion_flutter/lib/pagina_principal.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'dart:io';
 import 'producto_detalle.dart';
 import 'subir_producto_stateless.dart';
+import 'perfil.dart';
+import 'chat_screen.dart';
 
 class PaginaPrincipal extends StatefulWidget {
   const PaginaPrincipal({super.key});
@@ -12,6 +14,27 @@ class PaginaPrincipal extends StatefulWidget {
 
 class _PaginaPrincipalState extends State<PaginaPrincipal> {
   List<Map<String, dynamic>> productos = [];
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+
+    if (index == 1) {
+      // Navegar a la pantalla de chats
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const ChatScreen()),
+      );
+    } else if (index == 2) {
+      // Navegar a la pantalla del perfil
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const PerfilScreen()),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -73,6 +96,24 @@ class _PaginaPrincipalState extends State<PaginaPrincipal> {
           );
         },
         child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Inicio',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: 'Chats',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Perfil',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
     );
   }
